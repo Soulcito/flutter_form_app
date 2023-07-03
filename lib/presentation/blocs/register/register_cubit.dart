3e28de6a85afd732b89ebdd24af643971e4bc69a
Felipe <fepipe82@hotmail.com>
2023-07-02 21:53:45 -0400

    Fin de la seccion 20

---
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:forms_app/infrastructure/inputs/inputs.dart';
+import 'package:formz/formz.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterFormState> {
+  
+  RegisterCubit() : super(const RegisterFormState());
+
+  bool isValid() => Formz.validate([state.username, state.password, state.email]);
+
+  void onSubmit(){
+    emit(
+      state.copyWith(
+        formStatus: FormStatus.validating,
+        username: Username.dirty(state.username.value),
+        password: Password.dirty(state.password.value),
+        email: Email.dirty(state.email.value),
+        isValid: isValid()
+      )
+    );
+  }
+
+  void usernameChanged(String value) {
+    final username = Username.dirty(value);
+    emit(
+      state.copyWith(
+        username: username,
+        isValid: isValid()
+      )
+    );
+  }
+  void emailChanged(String value) {
+    final email = Email.dirty(value);
+    emit(
+      state.copyWith(
+        email: email,
+        isValid: isValid()
+      )
+    );
+  }
+  void passwordChanged(String value) {
+    final password = Password.dirty(value);
+    emit(
+      state.copyWith(
+        password: password,
+        isValid: isValid()
+      )
+    );
+  }
+
+}
